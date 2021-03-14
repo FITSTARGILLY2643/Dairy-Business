8d778c9448655bc75081043c699c09c6a2b39263
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-03-14 22:58:34 +0300

    change the items alighnment

--- report.js
+++ report.js
@@ -15,37 +15,37 @@ function show() {
 }
 
 
-// function production() {
-//     var shedA = (document.getElementById("one").value);
-//     var shedB = (document.getElementById("two").value);
-//     var shedC = (document.getElementById("three").value);
-//     var shedD = (document.getElementById("four").value);
+function production() {
+    var shedA = (document.getElementById("one").value);
+    var shedB = (document.getElementById("two").value);
+    var shedC = (document.getElementById("three").value);
+    var shedD = (document.getElementById("four").value);
 
-//     getElementById("one").innerHTML = shedA
-//     getElementById("two").innerHTML = shedB
-//     getElementById("three").innerHTML = shedC
-//     getElementById("four").innerHTML = shedD
+    getElementById("one").innerHTML = shedA
+    getElementById("two").innerHTML = shedB
+    getElementById("three").innerHTML = shedC
+    getElementById("four").innerHTML = shedD
 
 
-// }
-// production();
+}
+production();
 
-// function TotalProducton() {
-//     sum = shedA + shedB + shedC + shedD;
-//     document.write("Your total production is " + " " + sum + " litres per day.");
-//     return sum;
-// }
-// TotalProducton()
+function TotalProducton() {
+    sum = shedA + shedB + shedC + shedD;
+    document.write("Your total production is " + " " + sum + " litres per day.");
+    return sum;
+}
+TotalProducton()
 
-// incomePerDay = TotalProducton() * 45;
-// incomePerWeek = incomePerDay * 7;
-// incomePerYear = incomePerDay * 366;
-// document.write("Your daily income will be Ksh " + incomePerDay);
-// document.write("Your weekly income will be Ksh " + incomePerWeek);
-// document.write("Your yearly income will be Ksh " + incomePerYear);
-// var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-// var daymonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-// for (i = 0; i < months.length; i++) {
-//     sum = daymonth[i] * incomePerDay;
-//     document.write('Your income for ' + months[i] + ' is  Ksh' + sum)
-// }
+incomePerDay = TotalProducton() * 45;
+incomePerWeek = incomePerDay * 7;
+incomePerYear = incomePerDay * 366;
+document.write("Your daily income will be Ksh " + incomePerDay);
+document.write("Your weekly income will be Ksh " + incomePerWeek);
+document.write("Your yearly income will be Ksh " + incomePerYear);
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var daymonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+for (i = 0; i < months.length; i++) {
+    sum = daymonth[i] * incomePerDay;
+    document.write('Your income for ' + months[i] + ' is  Ksh' + sum)
+}
