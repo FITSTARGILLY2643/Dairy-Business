3972c443bd6819a1a0d19e6aebfef194f3e89cbd
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-03-14 18:34:42 +0300

    Link report.js to report html

---
+++ report.js
@@ -0,0 +1,22 @@
+var k = "The respective values are : <br>";
+var input = document.getElementsByName('array[]');
+
+function show() {
+
+
+    for (var i = 0; i < input.length; i++) {
+        var a = input[i];
+        k = k + a.value + " " + "<br>";
+    }
+
+
+    document.getElementById("par").innerHTML = k;
+    document.getElementById("po").innerHTML = "Output";
+}
+
+function production() {
+    var shedA = (document.getElementById("one").value);
+    var shedB = (document.getElementById("two").value);
+    var shedC = (document.getElementById("three").value);
+    var shedD = (document.getElementById("four").value);
+}
